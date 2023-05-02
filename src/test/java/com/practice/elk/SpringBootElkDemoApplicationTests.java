b385c2c0fd0018c6dc6313821efe2637c25dfaa0
Rahil <rahil@inexture.com>
2023-05-02 12:40:56 +0530

    springboot error log in elasticsearch using logstash

---
+++ src/test/java/com/practice/elk/SpringBootElkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
